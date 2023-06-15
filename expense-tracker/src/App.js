1673af1cb6fdb034f4b3ea12f0eec0cc3308b324
chintan <patelchintan843@gmail.com>
2023-06-15 17:54:59 +0530

    made background shadow

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./App.css"
+import Layout from './components/layout/Layout'
 
 const App = () => {
   return (
     <>
+      <div className="container-wrapper">
+        <Layout />
+        <div className="main-container">
+        </div>
+      </div>
     </>
   )
 }
---
+++ expense-tracker/src/components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./layout.css"
+
+const Layout = () => {
+    return (
+        <>
+            <div className='container-layout'>fd</div>
+        </>
+    )
+}
+
+export default Layout
